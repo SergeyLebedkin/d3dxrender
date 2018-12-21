71179e01c1b1ebd57018bfa579698111e1b593fe
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2018-12-21 17:40:45 +0200

    vulkan: add utils functions

--- build/vulkan/VulkanUtils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vulkan/vulkan.h>
-
-// Create Vulcan Handlers
-VkInstance VulkanCreateInstance();
-VkSurfaceKHR VulkanCreateSurface(VkInstance vkInstance, HWND hWnd);
-VkPhysicalDevice VulkanGetPhysicalDevice(VkInstance vkInstance);
-VkDevice VulkanCreateDevice(VkPhysicalDevice vkPhysicalDevice, VkSurfaceKHR vkSurfaceKHR);
